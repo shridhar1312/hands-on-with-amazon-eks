d3d364cc619615f44a51d3bb7a948628dd7e6d67
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2026-04-26 13:07:25 +0530

    Update create.sh

--- Infrastructure/k8s-tooling/external-dns/create.sh
+++ Infrastructure/k8s-tooling/external-dns/create.sh
@@ -1,2 +1,20 @@
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
-helm upgrade --install external-dns external-dns/external-dns
+
+helm repo update
+
+helm upgrade --install external-dns external-dns/external-dns \
+  --set provider.name=aws \
+  --set policy=upsert-only \
+  --set registry=txt \
+  --set txtOwnerId=eks-acg \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=${service_account_name} \
+  --set env[0].name=AWS_DEFAULT_REGION \
+  --set env[0].value=us-east-1
+
+
+
+
+
+# helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
+# helm upgrade --install external-dns external-dns/external-dns
