6839bbf7a6cf1ac531c8b1eb4f710707f8b8e8ab
Shridhar Rodge <56276945+shridhar1312@users.noreply.github.com>
2026-04-26 13:06:34 +0530

    Update create-irsa.sh

--- Infrastructure/k8s-tooling/external-dns/create-irsa.sh
+++ Infrastructure/k8s-tooling/external-dns/create-irsa.sh
@@ -6,7 +6,17 @@ eksctl create iamserviceaccount --name ${service_account_name} \
     --cluster eks-acg \
     --attach-policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --approve
 
-helm upgrade --install external-dns \
-    --set serviceAccount.create=false \
-    --set serviceAccount.name=${service_account_name} \
-    external-dns/external-dns
+# helm upgrade --install external-dns \
+#     --set serviceAccount.create=false \
+#     --set serviceAccount.name=${service_account_name} \
+#     external-dns/external-dns
+    
+helm upgrade --install external-dns external-dns/external-dns \
+  --set provider.name=aws \
+  --set policy=upsert-only \
+  --set registry=txt \
+  --set txtOwnerId=eks-acg \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=${service_account_name} \
+  --set env[0].name=AWS_DEFAULT_REGION \
+  --set env[0].value=us-east-1
